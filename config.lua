a56fcf7868b0fb7a4468ed2efc5e5871e3ddbfe3
TheRoPlayer <benjidaddy27@gmail.com>
2026-04-10 16:00:14 +0100

    Update config values for testing purposes

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 -- config.lua
 local Config = {
-    ["Key"] = "GEMINI_2026",
+    ["Key"] = "test",
     ["KeyLink"] = "https://linkvertise.com/yourlink",
     ["MainScriptURL"] = "https://raw.githubusercontent.com/User/Repo/main/protected.lua",
-    ["Title"] = "GEMINI <font color='#7850FF'>PROJECT</font>",
+    ["Title"] = "test hub <font color='#7850FF'>PROJECT</font>",
     ["Accent"] = Color3.fromRGB(120, 80, 255)
 }
 
 -- This fetches your Obfuscated Loader and "injects" the config into it
-local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/User/Repo/main/obfuscated_loader.lua"))()
+local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRoPlayer/HyperKeySystem/refs/heads/main/main.lua"))()
 Loader(Config)
